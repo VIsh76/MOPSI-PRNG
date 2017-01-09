18f0df098e9ee5fb61a31e9cc30db44c99195fac
Vish76 <victor.marchais@eleves.enpc.fr>
2017-01-09 19:23:14 +0100

    commencement de ziggurat

---
+++ ziggurat.cpp
@@ -0,0 +1,5 @@
+//
+// Created by victor on 08/01/17.
+//
+
+#include "ziggurat.h"
---
+++ ziggurat.h
@@ -0,0 +1,16 @@
+//
+// Created by victor on 08/01/17.
+//
+
+#ifndef PROJET_MOPSI_ZIGGURAT_H
+#define PROJET_MOPSI_ZIGGURAT_H
+
+
+
+class ziggurat {
+
+};
+
+
+
+#endif //PROJET_MOPSI_ZIGGURAT_H
