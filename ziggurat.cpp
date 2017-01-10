feb58d4af89ecc4a18ddacac847df41b90ae770b
Vish76 <victor.marchais@eleves.enpc.fr>
2017-01-10 13:48:23 +0100

    avancement de ziggurat

--- ziggurat.cpp
+++ ziggurat.cpp
@@ -3,10 +3,12 @@
 //
 
 
+
 #include "ziggurat.h"
 
-Pointeurs de fonctions
-/////////////////////////////
+
+
+//////////Pointeurs de fonctions///////
 int funtest(int a[dimension]){
     return a[0];
 }
@@ -15,15 +17,44 @@ int(*pointeur)(int[dimension]) = funtest;
 
 //////////////////////
 
+///////////////////Loi Normale/////////////
+
+double f_gaus(double x) {
+    return exp(-x * x)/(sqrt(2)*pi);
+}
+
+double f_1_gaus(double y) {
+    return sqrt(-log(sqrt(2*pi)*y));
+}
+///////////////////////////////
+
 
 vector<int> ziggurat(LCG l, int(*pointeur)(int[dimension])){
     return vector<int>(3);
 }
 
-Ziggurat::Ziggurat(int precision, float interval, float (*f)(float)) {
+Ziggurat::Ziggurat(int C, double R, double V, double (*f)(double)) {
     dim = 1;
-    interval = interval;
-    precision = precision;
-    for(int i=0; i<precision; i++){
+    intervale = R;
+    volume = V;
+    vector<double>table_x(C+1);
+    vector<double>table_y(C+1);
+    table_x[0]=V/f(R);
+    table_x[1]=R;
+    table_x[C]=0;
+    for(int i =2; i< C; i++){
+        table_x[i] = sqrt(-2 * log(V / table_x[i-1] + f(table_x[i-1])));
     }
+    for(int i=0; i<precision; i++){
+        table_y[i] = table_x[i + 1] / table_x[i];
+    }
+}
+
+//int convert_LCG_in_1_N(k_sur_m);
+
+// dR la limite basse de f
+// mieux vaut prendre M de lcg un multiple de C;
+double F_genere_Z(Ziggurat Z, LCG lcg){
+    int C=Z.getC();
+    A=lcg.run();
 }
--- ziggurat.h
+++ ziggurat.h
@@ -6,6 +6,9 @@
 #define PROJET_MOPSI_ZIGGURAT_H
 
 #include "LCG.h"
+#include <cmath>
+
+double const pi = 3.14;
 int const dimension=3;
 
 #include <vector>
@@ -13,13 +16,24 @@ int const dimension=3;
 using namespace std;
 class Ziggurat{
 private:
+    int C;
     int dim;
     int precision;
-    float intervale;
-    vector<float> table;
+    double intervale;
+    double volume;
+    vector<double> table_x;
+    vector<double> table_y;
 public:
-    Ziggurat(int precision, float interval, float(*f)(float) );
-
+    Ziggurat(int C, double R, double V, double (*f)(double));
+    int getC(){
+        return C;
+    }
+    double getX(int a){
+        return table_x[a];
+    }
+    double getY(int b){
+        return table_y[b];
+    }
 };
 
 
