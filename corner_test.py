735fedefd5459210baf3e4bc9f1fd7f145b12c9e
Joar Chouaki <joar.chouaki@eleves.enpc.fr>
2017-02-14 20:31:59 +0100

    Corner tests

--- corner_test.py
+++ corner_test.py
@@ -4,8 +4,8 @@
 import csv
 import matplotlib.pyplot as plt
 a=1
-crx=csv.reader(open("low_dis_1.csv","rb"))
-cry=csv.reader(open("low_dis_2.csv","rb"))
+crx=csv.reader(open("Corner_Please/low_dis_1.csv","rb"))
+cry=csv.reader(open("Corner_Please/low_dis_2.csv","rb"))
 def corner_test(a) :
 #    genx=[4,7,3,1,4,6,8,3]
 #    geny=[3,8,4,1,9,5,9,4]
@@ -15,7 +15,8 @@ def corner_test(a) :
         genx+=row
     for row in cry :
         geny+=row
+    #print genx[0]
     plt.axis([0,a,0,a]) #zoom sur la fenetre
     plt.scatter(genx,geny,c='black')
-
+    plt.show()
 corner_test(a)
